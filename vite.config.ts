d5a260f0412d36d758afe4b2b125aba50619046c
Felix Orinda <forinda82@gmail.com>
2023-01-29 09:35:22 +0300

    build: Change build destination

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: '../../dist-ui',
 		rollupOptions: {
 			output: {
 				manualChunks: {},
